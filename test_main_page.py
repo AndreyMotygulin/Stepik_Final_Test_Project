4edf0d32f95e883465ecda6655f48eafc668eee9
andrey.motygulin <andrey.motygulin.contractor@draeger.com>
2023-07-02 22:41:18 +0300

    locators.py and login_page.py created, test_main_page.py edited

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,19 @@
 from Stepik_Final_Test_Project.pages.main_page import MainPage
-import time
+from Stepik_Final_Test_Project.pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
     page = MainPage(browser, link)
-    
     page.open()
+
     page.go_to_login_page()
     page.should_be_login_link()
-    time.sleep(5)
+
+    login_page = LoginPage(browser, link)
+
+    login_page.should_be_login_url()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
