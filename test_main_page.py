f72ea88f26d843147915d92ee5e5b27d628656ba
andrey.motygulin <andrey.motygulin.contractor@draeger.com>
2023-07-03 08:16:45 +0300

    page transition to login page was added in the test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -4,16 +4,12 @@ from Stepik_Final_Test_Project.pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
     page = MainPage(browser, link)
     page.open()
-
-    page.go_to_login_page()
     page.should_be_login_link()
+    page.go_to_login_page()
 
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-    login_page.should_be_login_url()
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
