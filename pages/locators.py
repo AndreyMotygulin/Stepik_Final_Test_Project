48a8a93174b0c45016562479458bd17b1ac173a1
andrey.motygulin <andrey.motygulin.contractor@draeger.com>
2023-07-07 09:28:39 +0300

    product_page.py and test_product_page.py are added

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main>h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME_AFTER_ADDING = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) .alertinner strong")
+    PRODUCT_PRICE_AFTER_ADDING = (By.CSS_SELECTOR, "#messages .alert:nth-child(3) .alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+from Stepik_Final_Test_Project.pages.base_page import BasePage
+from Stepik_Final_Test_Project.pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        button_add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button_add_to_basket.click()
+
+    def get_product_name(self):
+        name_field = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        return name_field.text
+
+    def get_product_price(self):
+        price_field = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return price_field.text
+
+    def check_product_name_after_adding_to_basket(self):
+        name_field_after_adding_to_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_AFTER_ADDING)
+        expected_product_name = self.get_product_name()
+        actual_product_name = name_field_after_adding_to_basket.text
+        assert actual_product_name == expected_product_name
+
+    def check_product_price_after_adding_to_basket(self):
+        price_field_after_adding_to_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_AFTER_ADDING)
+        expected_product_price = self.get_product_price()
+        actual_product_price = price_field_after_adding_to_basket.text
+        assert actual_product_price == expected_product_price
